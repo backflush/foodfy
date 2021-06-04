ee4421950d627421e3595390efae460f81f0e1bf
Wesley Bernardes <bwesleysouza@gmail.com>
2021-06-04 09:30:54 -0300

    Finished the about controller creation and link

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,10 @@ const express = require("express");
 const routes = express.Router();
 
 const DashboardController = require("./controllers/DashboardController");
+const AboutController = require("./controllers/AboutController");
 
 routes.get("/", DashboardController.index);
+
+routes.get("/about", AboutController.index);
 
 module.exports = routes;
