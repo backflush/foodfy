2d8f14d74f7a204647898a94bbc0ef348836512b
Wesley Bernardes <bwesleysouza@gmail.com>
2021-06-07 17:08:37 -0300

    Created the recipes page

--- src/routes.js
+++ src/routes.js
@@ -4,9 +4,12 @@ const routes = express.Router();
 
 const DashboardController = require("./controllers/DashboardController");
 const AboutController = require("./controllers/AboutController");
+const RecipesController = require("./controllers/RecipesController");
 
 routes.get("/", DashboardController.index);
 
 routes.get("/about", AboutController.index);
+
+routes.get("/recipes", RecipesController.index);
 
 module.exports = routes;
