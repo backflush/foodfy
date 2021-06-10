6493a34aec3d4451e3a4b2650d3238492ba68a7e
Wesley Bernardes <bwesleysouza@gmail.com>
2021-06-10 12:23:12 -0300

    Finished the show/hide functionality in recipe-show

--- public/js/main.js
+++ public/js/main.js
@@ -44,3 +44,47 @@ const Modal = {
         document.querySelector(".modal-overlay").classList.remove("activeModal");
     }
 }
+
+const Toggle = {
+    Ingredients() {
+        const button = document.querySelector(".btn-show-ingredients");
+
+        const ingredients = document.querySelector(".ingredients-content");
+        
+        if (button.innerHTML === "ESCONDER") {
+            button.innerHTML = "MOSTRAR";
+            ingredients.classList.toggle("display");
+        } else {
+            button.innerHTML = "ESCONDER";
+            ingredients.classList.toggle("display");
+        }
+    },
+
+    Preparation() {
+        const button = document.querySelector(".btn-show-preparation");
+
+        const preparation = document.querySelector(".preparation-content");
+        
+        if (button.innerHTML === "ESCONDER") {
+            button.innerHTML = "MOSTRAR";
+            preparation.classList.toggle("display");
+        } else {
+            button.innerHTML = "ESCONDER";
+            preparation.classList.toggle("display");
+        }
+    },
+    
+    Information() {
+        const button = document.querySelector(".btn-show-information");
+
+        const information = document.querySelector(".information-content");
+        
+        if (button.innerHTML === "ESCONDER") {
+            button.innerHTML = "MOSTRAR";
+            information.classList.toggle("display");
+        } else {
+            button.innerHTML = "ESCONDER";
+            information.classList.toggle("display");
+        }
+    },
+}
