d5d27a5142e17dab7566035a46dd10d0e0805847
Wesley Bernardes <bwesleysouza@gmail.com>
2021-06-10 19:49:10 -0300

    Refactoring and adding data file

--- src/controllers/RecipesController.js
+++ src/controllers/RecipesController.js
@@ -1,9 +1,15 @@
+const data = require("../../data");
+
 module.exports = {
     index(req, res) {
-        res.render("recipes");
+        res.render("recipes", {
+            data: data
+        });
     },
 
     show(req, res) {
-        res.render("recipe-show");
+        res.render("recipe-show", {
+            data: data
+        });
     }
 }
